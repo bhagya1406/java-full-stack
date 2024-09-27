3bf3e7516201a5282cbc82bb5e145fa86650bde7
Bhagyashri <bsshintre77@gmail.com>
2024-09-27 22:38:50 +0530

    nested for loop codes adeed

---
+++ control statement/nested for loop/p15.java
@@ -0,0 +1,16 @@
+class Demo{
+	public static void main(String[]args){
+	char ch;
+		for(ch ='A'; ch<='D'; ch++){
+			for(int j = 1 ; j<=4 ; j++){
+				System.out.print(ch +" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+O/p:-	A A A A
+	B B B B
+	C C C C
+	D D D D
---
+++ control statement/nested for loop/p16.java
@@ -0,0 +1,15 @@
+class Demo{
+	public static void main(String[]args){
+		for(int i =1;i<=4;i++){
+			for(char ch1='A';ch1<='D';ch1++){
+				System.out.print(ch1+" ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+O/p:-   A B C D	
+	A B C D
+	A B C D
+	A B C D			
